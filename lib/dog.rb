1260cf6352630e3ee4d4b15571f044cc78688929
Rosa Carranza <rcarranza1024@yahoo.com>
2020-04-28 21:03:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
-# Add your code here
+class Dog
+  @@all = []
+
+  attr_accessor :puppy
+
+  def initialize(puppy)
+    @puppy = puppy
+    @@all << self
+  end
+
+  def self.all
+    @@all 
+  end
+end
